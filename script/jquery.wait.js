b33fdb5e740439e5d95e067db4120ed02d128f94
polcats <24451518+polcats@users.noreply.github.com>
2019-12-08 13:57:51 +0800

    Added initial working visualization for bubblesort.

---
+++ script/jquery.wait.js
@@ -0,0 +1,89 @@
+/**
+ * jquery.wait - insert simple delays into your jquery method chains
+ * @author Matthew Lee matt@madleedesign.com
+ */
+
+(function ($) {
+    function jQueryDummy ($real, delay, _fncQueue) {
+        // A Fake jQuery-like object that allows us to resolve the entire jQuery
+        // method chain, pause, and resume execution later.
+
+        var dummy = this;
+        this._fncQueue = (typeof _fncQueue === 'undefined') ? [] : _fncQueue;
+        this._delayCompleted = false;
+        this._$real = $real;
+
+        if (typeof delay === 'number' && delay >= 0 && delay < Infinity)
+            this.timeoutKey = window.setTimeout(function () {
+                dummy._performDummyQueueActions();
+            }, delay);
+
+        else if (delay !== null && typeof delay === 'object' && typeof delay.promise === 'function')
+            delay.then(function () {
+                dummy._performDummyQueueActions();
+            });
+
+        else if (typeof delay === 'string')
+            $real.one(delay, function () {
+                dummy._performDummyQueueActions();
+            });
+
+        else
+            return $real;
+    }
+
+    jQueryDummy.prototype._addToQueue = function(fnc, arg){
+        // When dummy functions are called, the name of the function and
+        // arguments are put into a queue to execute later
+
+        this._fncQueue.unshift({ fnc: fnc, arg: arg });
+
+        if (this._delayCompleted)
+            return this._performDummyQueueActions();
+        else
+            return this;
+    };
+
+    jQueryDummy.prototype._performDummyQueueActions = function(){
+        // Start executing queued actions.  If another `wait` is encountered,
+        // pass the remaining stack to a new jQueryDummy
+
+        this._delayCompleted = true;
+
+        var next;
+        while (this._fncQueue.length > 0) {
+            next = this._fncQueue.pop();
+
+            if (next.fnc === 'wait') {
+                next.arg.push(this._fncQueue);
+                return this._$real = this._$real[next.fnc].apply(this._$real, next.arg);
+            }
+
+            this._$real = this._$real[next.fnc].apply(this._$real, next.arg);
+        }
+
+        return this;
+    };
+
+    $.fn.wait = function(delay, _queue) {
+        // Creates dummy object that dequeues after a times delay OR promise
+
+        return new jQueryDummy(this, delay, _queue);
+    };
+
+    for (var fnc in $.fn) {
+        // Add shadow methods for all jQuery methods in existence.  Will not
+        // shadow methods added to jQuery _after_ this!
+        // skip non-function properties or properties of Object.prototype
+
+        if (typeof $.fn[fnc] !== 'function' || !$.fn.hasOwnProperty(fnc))
+            continue;
+
+        jQueryDummy.prototype[fnc] = (function (fnc) {
+            return function(){
+                var arg = Array.prototype.slice.call(arguments);
+                return this._addToQueue(fnc, arg);
+            };
+        })(fnc);
+    }
+})(jQuery);
---
+++ script/script.js
@@ -0,0 +1,117 @@
+var CONTAINER_WIDTH = 580;
+var container = document.getElementById("bars");
+function clearContainer() {
+    container.innerHTML = "";
+}
+
+var TOTAL_ELEMENTS = 10;
+function insertBars() {
+    var width = CONTAINER_WIDTH / TOTAL_ELEMENTS;
+
+    for (i = 0; i < TOTAL_ELEMENTS; ++i) {
+        var height = Math.round(Math.random() * 89) + 10;
+        var bar = document.createElement("div");
+        bar.setAttribute("class", "bar");
+        bar.setAttribute("style", "width: " + width + "px; height: " + height + "%;");
+        bar.innerHTML = height;
+        container.appendChild(bar);
+    }
+}
+
+function newArray() {
+    window.location.reload();
+}
+clearContainer();
+insertBars();
+
+function swapElements(obj1, obj2) {
+    // from https://stackoverflow.com/questions/10716986/swap-2-html-elements-and-preserve-event-listeners-on-them?lq=1
+    // save the location of obj2
+    var parent2 = obj2.parentNode;
+    var next2 = obj2.nextSibling;
+    // special case for obj1 is the next sibling of obj2
+    if (next2 === obj1) {
+        // just put obj1 before obj2
+        parent2.insertBefore(obj1, obj2);
+    } else {
+        // insert obj2 right before obj1
+        obj1.parentNode.insertBefore(obj2, obj1);
+
+        // now insert obj1 where obj2 was
+        if (next2) {
+            // if there was an element after obj2, then insert obj1 right before that
+            parent2.insertBefore(obj1, next2);
+        } else {
+            // otherwise, just append as last child
+            parent2.appendChild(obj1);
+        }
+    }
+}
+
+function getHeight(elem) {
+    return elem.style.height;
+}
+
+function redBg(elem1, elem2) {
+    elem1.style.backgroundColor = "red";
+    elem2.style.backgroundColor = "red";
+}
+
+function blueBg(elem1, elem2) {
+    elem1.style.backgroundColor = "lightblue";
+    elem2.style.backgroundColor = "lightblue";
+}
+
+var bars = document.getElementsByClassName("bar");
+function bubbleSor2t() {
+    for (i = 0; i < bars.length; ++i) {
+        for (j = 0; j < bars.length - i - 1; ++j) {
+            var leftElement = bars[j + 1];
+            var rightElement = bars[j];
+
+            $(leftElement).addClass("red");
+
+            if (getHeight(leftElement) > getHeight(rightElement)) {
+                swapElements(bars[j + 1], bars[j]);
+                bars = document.getElementsByClassName("bar");
+            }
+            $(leftElement)
+                .removeClass("red")
+                .wait(1000);
+        }
+    }
+}
+
+function bubbleSort() {
+    var delay = 100;
+    var outerDelay = delay * TOTAL_ELEMENTS;
+
+    for (i = 0; i < bars.length; ++i) {
+        (function(i) {
+            setTimeout(function() {
+                for (j = 0; j < bars.length - i - 1; ++j) {
+                    (function(j) {
+                        setTimeout(function() {
+                            var leftElement = bars[j];
+                            var rightElement = bars[j + 1];
+
+                            $(bars[j])
+                                .addClass("blue")
+                                .wait(delay)
+                                .removeClass("blue");
+
+                            $(bars[j + 1])
+                                .addClass("blue")
+                                .wait(delay)
+                                .removeClass("blue");
+
+                            if (getHeight(rightElement) > getHeight(leftElement)) {
+                                swapElements(rightElement, leftElement);
+                            }
+                        }, delay * j);
+                    })(j);
+                }
+            }, outerDelay * i);
+        })(i);
+    }
+}
