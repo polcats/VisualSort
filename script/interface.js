a0a8dc81b12ed7d86915b0ffb1b9521d68c0ecbf
polcats <24451518+polcats@users.noreply.github.com>
2019-12-12 06:53:05 +0800

    Reorganized files.

---
+++ script/interface.js
@@ -0,0 +1,81 @@
+// Initialize
+LoadUrlParams();
+
+function LoadUrlParams() {
+    const urlParams = new URLSearchParams(window.location.search);
+    const elements = parseInt(urlParams.get("elements"));
+    var error = false;
+
+    if (urlParams.has("delay")) {
+        const delay = parseInt(urlParams.get("delay"));
+        if (delay > 0 && delay <= 100) {
+            DELAY = delay;
+            document.getElementById("delay").value = DELAY;
+        } else {
+            error = true;
+        }
+    }
+
+    if (urlParams.has("elements")) {
+        if (elements >= 5 && elements <= 30) {
+            TOTAL_ELEMENTS = elements;
+            document.getElementById("elements").value = TOTAL_ELEMENTS;
+        } else {
+            error = true;
+        }
+    }
+
+    if (error) {
+        alert("Unable to load some parameters!");
+    }
+
+    updateDelay();
+    updateElements();
+}
+
+function updateDelay() {
+    DELAY = document.getElementById("delay").value;
+    document.getElementById("delay-count").innerHTML = DELAY;
+}
+
+function updateElements() {
+    clearContainer();
+    TOTAL_ELEMENTS = document.getElementById("elements").value;
+    document.getElementById("element-count").innerHTML = TOTAL_ELEMENTS;
+    insertBars();
+}
+
+function insertBars() {
+    const width = CONTAINER_WIDTH / TOTAL_ELEMENTS;
+
+    // generate unique values
+    var set = new Set();
+    while (set.size < TOTAL_ELEMENTS) {
+        set.add(Math.round(Math.random() * 99) + 1);
+    }
+
+    // generate bars
+    var arr = Array.from(set);
+    for (i = 0; i < arr.length; ++i) {
+        let bar = document.createElement("div");
+        bar.setAttribute("class", "bar");
+        bar.setAttribute("style", "width: " + width + "px; height: " + arr[i] + "%;");
+        bar.innerHTML = arr[i];
+        container.appendChild(bar);
+    }
+}
+
+function disableInput(what = true) {
+    $(".sort").attr("disabled", what);
+    $(".sort").removeClass("green");
+    $(".slider-input").attr("disabled", what);
+    $("#reset").addClass("green");
+}
+
+function clearContainer() {
+    container.innerHTML = "";
+}
+
+function reset() {
+    window.location.href = "index.html?delay=" + DELAY + "&elements=" + TOTAL_ELEMENTS;
+}
