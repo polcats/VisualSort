7b3450f712984c33c6a31e6294858e56f2e6c9f8
polcats <24451518+polcats@users.noreply.github.com>
2019-12-12 20:29:58 +0800

    Separated solution generation and animation.

--- script/animation.js
+++ script/animation.js
@@ -1,5 +1,4 @@
-function generateElements(count)
-{
+function generateElements(count) {
     // generate unique values
     var set = new Set();
     while (set.size < count) {
@@ -9,83 +8,77 @@ function generateElements(count)
     return Array.from(set);
 }
 
-function animate(anim)
-{
-    for(i = anim.moves.size - 1; i >= 0; --i)
-    {
-        // hightlight elements
-        let highlight = anim.moves[i].highlight;
-
-        // run timeout
-        for (h = 0; h < highlight.size; ++h)
-        {
-            bars[highlight[0]].addClass(highlight[1]);
-        }
-
-        // swap elements
-        let elem = anim.moves[i].elements;
-        $(elem[0]).swap(elem[1]);
-
-        // remove highlight
-        for (h = 0; h < highlight.size; ++h)
-        {
-            bars[highlight[0]].removeClass(highlight[1]);
-        }
+function animate(origin, solution) {
+    for (i = 0; i < solution.moves.length; ++i) {
+        let highlight = solution.moves[i].highlight;
+        (function(solution, i, bars) {
+            setTimeout(function() {
+                $(".bar").removeClass("compared");
+                for (h = 0; h < highlight.length; ++h) {
+                    $(bars[highlight[0]]).addClass(highlight[1]);
+                }
+                let elem = solution.moves[i].elements;
+                $(bars[elem[0]]).swap(bars[elem[1]]);
+            }, 500 * i);
+            // remove highlight
+        })(solution, i, bars);
     }
 }
 
-function solve(origin, solution)
-{
-    console.log("origin : " + origin);
-    for (i = 0; i < solution.moves.length; ++i)
-    {
-        let temp = origin[solution.moves[i].elements[0]];
-        origin[solution.moves[i].elements[0]] = origin[solution.moves[i].elements[1]] ;
-        origin[solution.moves[i].elements[1]] = temp;
-        console.log(" - " + origin);
-    }
-    console.log("solved : " + origin);
-}
-
-function insertionSort(e)
-{
+function insertionSort(e) {
     var elements = e;
     var solutionObject = {};
     solutionObject.moves = [];
 
-    for (i = 1; i < elements.length; ++i)
-    {
+    for (i = 1; i < elements.length; ++i) {
         var key = elements[i];
         var j = i - 1;
-        while (j >= 0 && elements[j] < key)
-        {
-            elements[j+1] = elements[j];
+        while (j >= 0 && elements[j] < key) {
+            elements[j + 1] = elements[j];
 
-            move =
-            {
-                highlight : [],
-                elements : []
+            move = {
+                highlight: [],
+                elements: []
             };
 
-            move.elements.push(j+1);
+            move.highlight.push([j + 1], "compared");
+            move.highlight.push([j, "compared"]);
+
+            move.elements.push(j + 1);
             move.elements.push(j);
             solutionObject.moves.push(move);
 
-            j = j -1;
+            j = j - 1;
         }
-        elements[j+1] = key;
+        elements[j + 1] = key;
     }
 
-    console.log("solution : " + elements)
+    console.log("solution : " + elements);
 
     return solutionObject;
 }
 
-function test()
-{
+function test() {
     var e = generateElements(10);
-    console.log("initial : " + e)
+    console.log("initial : " + e);
     var newInstance = JSON.parse(JSON.stringify(e));
     var r = insertionSort(newInstance);
     solve(e, r);
+    return 0;
+}
+
+function getElements() {
+    var els = Array();
+    for (i = 0; i < bars.length; ++i) {
+        els.push(parseInt(bars[i].innerHTML));
+    }
+
+    return els;
+}
+
+function runAlgo() {
+    var origin = getElements();
+    var origin_copy = JSON.parse(JSON.stringify(origin));
+    var solution = insertionSort(origin_copy);
+    animate(origin, solution);
 }
