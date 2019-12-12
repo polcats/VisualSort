1c5967875ea954d81a2c0075dacee7e4ce820504
polcats <24451518+polcats@users.noreply.github.com>
2019-12-12 22:00:33 +0800

    Adjusted animations; Added new bubble sort algo using the new format.

--- script/animation.js
+++ script/animation.js
@@ -10,24 +10,27 @@ function generateElements(count) {
 
 function animate(origin, solution) {
     for (i = 0; i < solution.moves.length; ++i) {
-        let highlight = solution.moves[i].highlight;
         (function(solution, i, bars) {
             setTimeout(function() {
-                for (h = 0; h < highlight.length; ++h) {
-                    $(bars[highlight[0]])
-                        .addClass(highlight[1])
-                        .wait(50)
-                        .removeClass("compared");
-                }
+                let highlight = solution.moves[i].highlight;
 
                 let elem = solution.moves[i].elements;
+                $(bars[elem[0]])
+                    .addClass("compared")
+                    .wait(100)
+                    .removeClass("compared");
+                $(bars[elem[1]])
+                    .addClass("compared")
+                    .wait(100)
+                    .removeClass("compared");
+
                 $(bars[elem[0]]).swap(bars[elem[1]]);
-            }, DELAY * i);
+            }, DELAY * TOTAL_ELEMENTS * i);
         })(solution, i, bars);
     }
 }
 
-function insertionSort(e) {
+function insertion(e) {
     var elements = e;
     var solutionObject = {};
     solutionObject.moves = [];
@@ -43,8 +46,8 @@ function insertionSort(e) {
                 elements: []
             };
 
-            move.highlight.push(j + 1, "compared");
             move.highlight.push(j, "compared");
+            move.highlight.push(j + 1, "compared");
 
             move.elements.push(j + 1);
             move.elements.push(j);
@@ -55,6 +58,34 @@ function insertionSort(e) {
         elements[j + 1] = key;
     }
 
+    return solutionObject;
+}
+
+function bubble(e) {
+    var elements = e;
+    var solutionObject = {};
+    solutionObject.moves = [];
+
+    for (i = 0; i < elements.length; ++i) {
+        for (j = 0; j < elements.length - i - 1; ++j) {
+            move = {
+                highlight: [],
+                elements: []
+            };
+
+            move.highlight.push(j, "compared");
+            move.highlight.push(j + 1, "compared");
+
+            if (elements[j] < elements[j + 1]) {
+                var temp = elements[j];
+                elements[j] = elements[j + 1];
+                elements[j + 1] = temp;
+                move.elements.push(j);
+                move.elements.push(j + 1);
+                solutionObject.moves.push(move);
+            }
+        }
+    }
     return solutionObject;
 }
 
@@ -70,6 +101,7 @@ function getElements() {
 function runAlgo() {
     var origin = getElements();
     var origin_copy = JSON.parse(JSON.stringify(origin));
-    var solution = insertionSort(origin_copy);
+    var solution = bubble(origin_copy);
+    console.log(solution);
     animate(origin, solution);
 }
